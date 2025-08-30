808b38b3d6cb7bd117cbd2aed88b2f0a8bd31d8d
ABHISHEK DAS <abhishek.das.docker@gmail.com>
2025-08-30 16:54:24 +0530

    added models for jokes

---
+++ src/model.py
@@ -0,0 +1,18 @@
+class Joke:
+    def __init__(self,id,type,setup,punchline,length):
+        self.__id = id
+        self.__type = type
+        self.__setup = setup
+        self.__punchline = punchline
+        self.__length = len(setup)+len(punchline)
+        
+    def to_dict(self):
+        return {
+            "id" : self.__id,
+            "type" : self.__type,
+            "setup" : self.__setup,
+            "punchline" : self.__punchline,
+        }    
+    
+    def __str__(self):
+        return "ID :: "+str(self.__id)+" Type :: "+str(self.__type)+" Setup :: "+str(self.__setup)    
