f2477aca595b87881925ae4871c4cb016c488b0a
ABHISHEK DAS <abhishek.das.docker@gmail.com>
2025-08-30 16:39:47 +0530

    m

--- src/api_handler.py
+++ src/api_handler.py
@@ -16,38 +16,38 @@ class APIHandler:
             return requests.get(self.__getRandomUrl(1)).json()
 
         except requests.exceptions.HTTPError as http_err:
-            print(f"❌ HTTP error occurred: {http_err}")
+            print(f"HTTP error occurred: {http_err}")
         except requests.exceptions.ConnectionError:
-            print("❌ Connection error. Please check your network.")
+            print("Connection error. Please check your network.")
         except requests.exceptions.Timeout:
-            print("❌ Request timed out.")
+            print("Request timed out.")
         except requests.exceptions.RequestException as err:
-            print(f"❌ An error occurred: {err}")
+            print(f"An error occurred: {err}")
     
     def fetch_multiple_jokes(self,count):
         try:
             return requests.get(self.__getRandomUrl(count)).json()
 
         except requests.exceptions.HTTPError as http_err:
-            print(f"❌ HTTP error occurred: {http_err}")
+            print(f"HTTP error occurred: {http_err}")
         except requests.exceptions.ConnectionError:
-            print("❌ Connection error. Please check your network.")
+            print("Connection error. Please check your network.")
         except requests.exceptions.Timeout:
-            print("❌ Request timed out.")
+            print("Request timed out.")
         except requests.exceptions.RequestException as err:
-            print(f"❌ An error occurred: {err}")
+            print(f"An error occurred: {err}")
             
     def fetch_joke_by_id(self,joke_id):
         try:
             return requests.get(self.__getUrlByID(joke_id)).json()
 
         except requests.exceptions.HTTPError as http_err:
-            print(f"❌ HTTP error occurred: {http_err}")
+            print(f"HTTP error occurred: {http_err}")
         except requests.exceptions.ConnectionError:
-            print("❌ Connection error. Please check your network.")
+            print("Connection error. Please check your network.")
         except requests.exceptions.Timeout:
-            print("❌ Request timed out.")
+            print("Request timed out.")
         except requests.exceptions.RequestException as err:
-            print(f"❌ An error occurred: {err}")     
+            print(f"An error occurred: {err}")     
     
         
